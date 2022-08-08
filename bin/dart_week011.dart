0955ee28f867bae0b3cf15e8a681921ba120c0e4
SombatBuekeaw <sombat073855@gmail.com>
2022-08-08 10:54:35 +0700

    first commit

---
+++ bin/dart_week011.dart
@@ -0,0 +1,7 @@
+void main(){
+  //Hello 5 times
+  for(int i = 1; i <= 4;i=i+1)
+  {
+    print('hello:$i');
+  }
+}
---
+++ test/dart_week011_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_week011/dart_week011.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
